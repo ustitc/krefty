d972a6c99eed6f1961d0a46ac35b051852848afd
Ruslan Ustits <ustitsruslan@gmail.com>
2021-09-27 23:06:37 +0300

    fix version

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,15 +7,17 @@ repositories {
 }
 
 group = "dev.ustits"
-version = "1.0.0-SNAPSHOT"
+version = "0.1.0-SNAPSHOT"
 
 tasks.withType<Test> {
     useJUnitPlatform()
 }
 
+val kotestVersion = "4.6.3"
+
 dependencies {
-    testImplementation("io.kotest:kotest-runner-junit5:4.6.3")
-    testImplementation("io.kotest:kotest-assertions-core:4.6.3")
-    testImplementation("io.kotest:kotest-property:4.6.3")
+    testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+    testImplementation("io.kotest:kotest-property:$kotestVersion")
 }
 
