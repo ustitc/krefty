c72ed32c71b832383cb54eb30954b900e988b228
Ruslan Ustits <ustitsruslan@gmail.com>
2021-10-31 22:26:44 +0300

    made refined types with equal values to be equal

--- krefty-core/src/commonMain/kotlin/dev/ustits/krefty/core/EagerRefined.kt
+++ krefty-core/src/commonMain/kotlin/dev/ustits/krefty/core/EagerRefined.kt
@@ -16,4 +16,20 @@ internal class EagerRefined<P : Predicate<T>, T> private constructor(private val
 
     override val unrefined: T
         get() = value
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as EagerRefined<*, *>
+
+        if (value != other.value) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return value?.hashCode() ?: 0
+    }
+
 }
--- krefty-core/src/commonTest/kotlin/dev/ustits/krefty/core/EagerRefinedTest.kt
+++ krefty-core/src/commonTest/kotlin/dev/ustits/krefty/core/EagerRefinedTest.kt
@@ -2,6 +2,7 @@ package dev.ustits.krefty.core
 
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.shouldBe
 import io.kotest.property.checkAll
 import io.kotest.property.forAll
 
@@ -20,6 +21,24 @@ class EagerRefinedTest : StringSpec({
                 EagerRefined(Predicate.Stub(false), it)
             }
         }
+    }
+
+    "refined types with equal values are equal" {
+        forAll<String> {
+            val predicate = Predicate.Stub<String>(true)
+            val first = EagerRefined(predicate, it)
+            val second = EagerRefined(predicate, it)
+
+            first == second
+        }
+    }
+
+    "refined types with different values are not equal" {
+        val predicate = Predicate.Stub<Int>(true)
+        val first = EagerRefined(predicate, 10)
+        val second = EagerRefined(predicate, 15)
+
+        (first != second) shouldBe true
     }
 
 })
