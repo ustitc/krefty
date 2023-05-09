9cdd02602ad03cc386a26f8b67c50be0f27aba6b
Ruslan Ustits <ustitsruslan@gmail.com>
2023-05-09 09:57:49 +0400

    Add snapshot job

--- buildSrc/src/main/kotlin/Ci.kt
+++ buildSrc/src/main/kotlin/Ci.kt
@@ -1,14 +1,6 @@
 object Ci {
 
-    private const val snapshotBase = "0.1.0"
-
-    private val githubRunNumber = System.getenv("GITHUB_RUN_NUMBER")
-
-    private val snapshotVersion = when (githubRunNumber) {
-        null -> "$snapshotBase-LOCAL"
-        else -> "$snapshotBase.${githubRunNumber}-SNAPSHOT"
-    }
-
+    private val snapshotVersion = System.getenv("SNAPSHOT_VERSION")?.let { "$it-SNAPSHOT" } ?: "0.0.1-LOCAL"
     private val releaseVersion = System.getenv("RELEASE_VERSION")
 
     val isRelease = releaseVersion != null
