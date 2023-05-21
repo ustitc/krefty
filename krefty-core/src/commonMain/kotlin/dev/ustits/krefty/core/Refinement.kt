2a44c899881a32d51680613e16e443d619e7693e
Ruslan Ustits <ustitsruslan@gmail.com>
2023-05-21 22:05:50 +0400

    Remove Refined

--- krefty-core/src/commonMain/kotlin/dev/ustits/krefty/core/Refinement.kt
+++ krefty-core/src/commonMain/kotlin/dev/ustits/krefty/core/Refinement.kt
@@ -1,21 +1,5 @@
 package dev.ustits.krefty.core
 
-@Deprecated(message = "Will be replaced by Refinement")
-interface Refined<P : Predicate<T>, T> {
-
-    @Deprecated("Use getOrThrow() instead")
-    val unrefined: T
-
-    fun getOrElse(block: (T) -> T): T
-
-    fun getOrThrow(): T
-
-    fun getOrNull(): T?
-
-    fun getOrError(): Result<T>
-
-}
-
 interface Refinement<T> {
 
     fun getOrElse(block: () -> T): T
