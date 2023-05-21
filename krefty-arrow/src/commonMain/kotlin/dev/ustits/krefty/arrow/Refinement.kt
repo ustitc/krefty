c3118e1d659a0bffdf3d3cfb676efec609b9a290
Ruslan Ustits <ustitsruslan@gmail.com>
2023-05-21 17:25:49 +0400

    Remove refine arrow dsl

--- krefty-arrow/src/commonMain/kotlin/dev/ustits/krefty/arrow/Refinement.kt
+++ krefty-arrow/src/commonMain/kotlin/dev/ustits/krefty/arrow/Refinement.kt
@@ -2,26 +2,8 @@ package dev.ustits.krefty.arrow
 
 import arrow.core.Either
 import arrow.core.left
-import arrow.core.raise.Raise
-import arrow.core.raise.fold
 import arrow.core.right
-import dev.ustits.krefty.core.Predicate
 import dev.ustits.krefty.core.Refinement
-
-@DslMarker annotation class RefineDsl
-
-@RefineDsl
-fun <A, B, Error> refine(value: A, block: RefinementRaise<A, Error>.() -> B): Either<Error, B> {
-    return fold({ block(RefinementRaise(value, this)) }, { Either.Left(it) }, { Either.Right(it) })
-}
-
-class RefinementRaise<A, Error>(val value: A, private val raise: Raise<Error>) : Raise<Error> by raise {
-
-    @RefineDsl
-    fun ensure(predicate: Predicate<A>, error: () -> Error): A {
-        return if (predicate.isRefined(value)) value else raise(error())
-    }
-}
 
 object RefinementError
 
--- krefty-arrow/src/commonTest/kotlin/dev/ustits/krefty/arrow/RefinementTest.kt
+++ krefty-arrow/src/commonTest/kotlin/dev/ustits/krefty/arrow/RefinementTest.kt
@@ -1,58 +1,19 @@
 package dev.ustits.krefty.arrow
 
-import arrow.core.raise.ensure
-import dev.ustits.krefty.predicate.string.NotBlank
+import dev.ustits.krefty.core.refine
 import io.kotest.assertions.arrow.core.shouldBeLeft
 import io.kotest.assertions.arrow.core.shouldBeRight
 import io.kotest.core.spec.style.StringSpec
-import kotlin.jvm.JvmInline
 
 class RefinementTest : StringSpec({
 
-    "returns right if matches condition" {
-        val result = refine("Krefty") {
-            ensure(value.isNotBlank()) { BlankString }
-            NotBlankString(value)
-        }
-        result shouldBeRight NotBlankString("Krefty")
-    }
-
-    "returns left if doesn't match condition" {
-        val result = refine("") {
-            ensure(value.isNotBlank()) { BlankString }
-            NotBlankString(value)
-        }
-        result shouldBeLeft BlankString
-    }
-
-    "returns right if matches predicate" {
-        val result = refine("Krefty") {
-            ensure(NotBlank()) { BlankString }
-            NotBlankString(value)
-        }
-        result shouldBeRight NotBlankString("Krefty")
-    }
-
-    "returns left if doesn't match predicate" {
-        val result = refine("") {
-            ensure(NotBlank()) { BlankString }
-            NotBlankString(value)
-        }
-        result shouldBeLeft BlankString
-    }
-
     "returns right" {
-        val result = dev.ustits.krefty.core.refine(NotBlank(), "test").getOrEither()
+        val result = refine("test", String::isNotBlank).getOrEither()
         result shouldBeRight "test"
     }
 
     "returns left" {
-        val result = dev.ustits.krefty.core.refine(NotBlank(), "").getOrEither()
+        val result = refine("", String::isNotBlank).getOrEither()
         result shouldBeLeft RefinementError
     }
 })
-
-object BlankString
-
-@JvmInline
-value class NotBlankString(private val value: String)
